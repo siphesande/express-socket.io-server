c2548dec8a06a70cd6d332b90cb607759b02f82e
siphesande <sphe.malgas@gmail.com>
2019-07-28 01:31:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 var express = require('express');var cors = require('cors')
-var app = express()
-app.use(cors())
+var app     = express()
 var server  = app.listen(8000);
 var io      = require('socket.io').listen(server);
+app.use(cors())
 
  
-
-
-
-
 app.get('/', (req, res) => {
-   
    res.send('Hello this is REST API')
 })
-
 
 io.on('connection', function(socket){
    console.log('user connected')
 
-  socket.on('disconnect', function(){
+   socket.on('disconnect', function(){
      console.log('user disconnected')
   })
 
